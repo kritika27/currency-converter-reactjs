cb8f36b79c1161c52d6570ba0d7f02d9cd0f4789
kritika <kritikasri27@gmail.com>
2021-02-02 11:22:50 +0530

    Sorted currencies in alphabetical order

--- src/App.js
+++ src/App.js
@@ -10,20 +10,33 @@ function App() {
   const [toCurrency, setToCurrency] = useState();
   const [exchangeRate, setExchangeRate] = useState();
   const [amount, setAmount] = useState(1);
-  //const [toAmount, setToAmount] = useState(1);
 
   useEffect(() => {
     fetch(BASE_URL)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        var key = Object.keys(data.rates).sort(function order(key1, key2) {
+          if (key1 < key2) return -1;
+          else if (key1 > key2) return +1;
+          else return 0;
+        });
+
+        // Taking the object in 'temp' object
+        // and deleting the original object.
+        var temp = {};
+        for (var i = 0; i < key.length; i++) {
+          temp[key[i]] = data.rates[key[i]];
+          delete data.rates[key[i]];
+        }
+
+        for (i = 0; i < key.length; i++) {
+          data.rates[key[i]] = temp[key[i]];
+        }
+
         const firstCurrency = Object.keys(data.rates)[0];
-
         setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
-
         setFromCurrency(data.base);
         setToCurrency(firstCurrency);
-
         setExchangeRate(data.rates[firstCurrency]);
       });
   }, []);
